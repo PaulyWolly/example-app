ee6437b76d175b7d398df951dd9211f0cb64e19b
Paul Welby <pwelby@gmail.com>
2021-10-19 12:51:19 -0700

    updated files

---
+++ src/app/cart.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+
+  constructor() { }
+}
