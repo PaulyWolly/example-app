5fbebd76cebfee821e158bbe221852e0e1a43dfa
Paul Welby <pwelby@gmail.com>
2021-10-19 20:18:00 -0700

    adding additional files for Angular example-app

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
 selector: 'product-details-component',
@@ -11,18 +12,25 @@ export class ProductDetailsComponent implements OnInit {
 
     product: Product | undefined;
 
-    constructor(private route: ActivatedRoute) {
-    }
+        constructor(
+            private route: ActivatedRoute,
+            private cartService: CartService
+        ) { }
 
-    ngOnInit(){
-        // First get the product id from the current route.
-            const routeParams = 
-        this.route.snapshot.paramMap;
-            const productIdFromRoute = 
-        Number(routeParams.get('productId'));
+        ngOnInit(){
+            // First get the product id from the current route.
+                const routeParams =
+            this.route.snapshot.paramMap;
+                const productIdFromRoute =
+            Number(routeParams.get('productId'));
 
-        //Find the product that corresponds with the id provided in route
-        this.product = products.find(product => 
-            product.id === productIdFromRoute)
-    }
+            //Find the product that corresponds with the id provided in route
+            this.product = products.find(product =>
+                product.id === productIdFromRoute)
+        }
+
+        addToCart(product: Product) {
+            this.cartService.addToCart(product);
+            window.alert('Your product has been addedd to the cart!');
+        }
 }
